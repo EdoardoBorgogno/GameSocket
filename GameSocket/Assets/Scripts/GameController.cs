5c97dd568ec6420119a420c290d0229498cb4bd7
Blacksdow <paolobruno2003@gmail.com>
2022-12-08 21:43:01 +0100

    Update x
    21.42

--- GameSocket/Assets/Scripts/GameController.cs
+++ GameSocket/Assets/Scripts/GameController.cs
@@ -7,12 +7,9 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+   
 }
